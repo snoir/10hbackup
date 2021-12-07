9fa82faa45f8b76f553f65a1fb716be7787688bd
Samir Noir <samir@noir.im>
2021-12-07 22:05:15 +0100

    Download data with libcurl from api.deezer.com

--- 10hbackup.c
+++ 10hbackup.c
@@ -1,5 +1,16 @@
 #include <curl/curl.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BASE_DEEZER_URI "api.deezer.com"
+
+struct http_data {
+	size_t size;
+	char *data;
+};
+
+size_t
+deezer_callback(char *data, size_t size, size_t nmemb, struct http_data *userdata);
 
 int
 main()
@@ -7,5 +18,41 @@ main()
 	curl_global_init(CURL_GLOBAL_ALL);
 	CURL *curl = curl_easy_init();
 
+	CURLcode res;
+	char err_buff[CURL_ERROR_SIZE];
+	struct http_data deezer_data;
+	deezer_data.data = malloc(1);
+	deezer_data.size = 0;
+
+	curl_easy_setopt(curl, CURLOPT_URL, BASE_DEEZER_URI);
+	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, err_buff);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &deezer_callback);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &deezer_data);
+
+	res = curl_easy_perform(curl);
+
+	if (res != CURLE_OK) {
+		fprintf(stderr, "Error occured:\n");
+		fprintf(stderr, "  %s\n", err_buff);
+		return EXIT_FAILURE;
+	} else {
+		printf("%s\n", deezer_data.data);
+	}
+
+	curl_easy_cleanup(curl);
+	curl_global_cleanup();
+
 	return EXIT_SUCCESS;
+}
+
+size_t
+deezer_callback(char *data, size_t size, size_t nmemb, struct http_data *userdata) {
+	int realsize = size * nmemb;
+
+	userdata->data = realloc(userdata->data, userdata->size + realsize);
+
+	memcpy(&userdata->data[userdata->size], data, realsize);
+	userdata->size += realsize;
+
+	return realsize;
 }
--- Makefile
+++ Makefile
@@ -1,2 +1,10 @@
-all:
+all: debug
+
+debug:
+	clang -g -Wall -Wextra -Werror -o 10hbackup 10hbackup.c -lcurl
+
+debug-allow-warn:
+	clang -g -Wall -Wextra -o 10hbackup 10hbackup.c -lcurl
+
+release:
 	clang -Wall -Wextra -Werror -o 10hbackup 10hbackup.c -lcurl
